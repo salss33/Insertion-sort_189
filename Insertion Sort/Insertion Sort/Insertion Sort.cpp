8df53fb3e1d6b6ffa7e808775d8b84882b250ced
salss33 <hallosalsa03@gmail.com>
2023-03-24 20:35:31 +0700

    procedure insertionsort

--- Insertion Sort/Insertion Sort/Insertion Sort.cpp
+++ Insertion Sort/Insertion Sort/Insertion Sort.cpp
@@ -7,25 +7,55 @@ int n;                                           // Membuat variable inputan n
 void input() {                 // Procedure input
 	while (true)
 	{
-		cout << "Masukan Jumlah Data Pada Array : "; // Membuat inputan jumlah element Array
-		cin >> n;                                    // Memanggil variable inputan n
+		cout << "Masukan Jumlah Data pada Array : "; // Membuat inputan jumlah element Array 
+		cin >> n;                                    // memanggil variable inputan n
 
-		if (n <= 20) {                                // Membuat kondisi n tidak lebih dari 20
+		if (n <= 20) {                               // Membuat kondisi n tidak lebih dari 20
 			break;
 		}
 		else
 		{
-			cout << "\nArray yang anda masukan maksimal 20 elemen.\n";    // Menampilkan pesan jika data lebih dari 20
+			cout << "\nArray yang anda masukan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
 		}
 	}
-	cout << endl;                                    // Membuat jarak per baris program
-	cout << "==============" << endl;                // Membuat tampilan susunan data element array
-	cout << "Masukan Element Array" << endl; 
-	cout << "==============" << endl;
+	cout << endl;                                 // Membuat jarak per baris program
+	cout << "=====================" << endl;     // Membuat tampilan susunan data element array
+	cout << "Masukan Element Array" << endl;
+	cout << "=====================" << endl;
 
 
-	for (int i = 0; i < n; i++)                      // Memnggunakan perulangan for untuk menyimpan data pada array
+	for (int i = 0; i < n; i++)                 // Memnggunakan perulangan for untuk menyimpan  data pada array
 	{
-		cout << "Data ke-" << (i + 1) << ": ";       // Memasukan atau menginputkan nilai data n
-		cin >> arr[i];                               // Menyimpan nilai data n kedalam array arr
+		cout << "Data ke-" << (i + 1) << ": ";  // Memasukan atau menginputkan nilai data n
+		cin >> arr[i];                          // Menyimpan nilai data n kedalam array arr
 	}
+}
+
+void insertionsort() {                 // Procedure insertiontsort
+
+	int temp;                         // Membuat variable data temporer atau penyimpanan sementara
+	int j;                            // Membuat variable j sebagai penanda
+
+	for (int i = 1; i < n; i++) { // 1. Looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];            // 2. simpan nilai arr[i] ke variable sementara temp
+
+		j = i - 1;                // 3. setting nilai j sama dengan i-1;
+
+		while (j >= 0 && arr[j] > temp) // 4. Looping while dimana nilai j lebih besar sama dengan 0 dan
+			// arr[j] lebih besar daripada temp
+		{
+			arr[j + 1] = arr[j];        // 4a. simpan arr[j] ke dalam variable arr[j+1] 
+			j--;                        // 4b. Decrement nilai j by 1
+		}
+
+
+		
+
+
+
+
+		 
+
+
+		 
