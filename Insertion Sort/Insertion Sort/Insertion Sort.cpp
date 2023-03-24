647049814652a4b0e734d8af6b4d19c6d22d7f89
salss33 <hallosalsa03@gmail.com>
2023-03-24 18:42:04 +0700

    Menampilkan pesan jika data lebih dari 20

--- Insertion Sort/Insertion Sort/Insertion Sort.cpp
+++ Insertion Sort/Insertion Sort/Insertion Sort.cpp
@@ -13,4 +13,6 @@ void input() {                 // Procedure input
 		if (n <= 20) {                                // Membuat kondisi n tidak lebih dari 20
 			break;
 		}
-		
+		else
+		{
+			cout << "\nArray yang anda masukan maksimal 20 elemen.\n";    // Menampilkan pesan jika data lebih dari 20
